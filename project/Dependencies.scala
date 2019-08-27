611ef95bbacb88ad26619cb6507fd721fd5fc787
Juan Marin Otero <juan.marin.otero@gmail.com>
2019-08-27 08:27:40 -0400

    Configure S3 server in test

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -1,6 +1,13 @@
 import sbt._
 
 object Dependencies {
+
+  val repos = Seq(
+    "Local Maven Repo" at "file://" + Path.userHome.absolutePath + "/.m2/repository",
+    "Typesafe Repo" at "http://repo.typesafe.com/typesafe/releases/",
+    "Sonatype releases" at "https://oss.sonatype.org/content/repositories/releases"
+  )
+
   lazy val logback = "ch.qos.logback" % "logback-classic" % Version.logback
   lazy val scalatest = "org.scalatest" %% "scalatest" % Version.scalatest
   lazy val akkaHttp = "com.typesafe.akka" %% "akka-http" % Version.akkaHttp
