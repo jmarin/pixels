6b49295606f89061ac0b2255d58e0532d97a8265
Juan Marin Otero <juan.marin.otero@gmail.com>
2019-09-04 08:41:20 -0400

    Add download image from S3 (incomplete)

--- common/src/main/scala/pixels/s3/S3Utils.scala
+++ common/src/main/scala/pixels/s3/S3Utils.scala
@@ -10,6 +10,7 @@ import akka.stream.alpakka.s3.ObjectMetadata
 import akka.NotUsed
 import akka.http.scaladsl.model.ContentType
 import akka.http.scaladsl.model.ContentTypes
+import akka.http.scaladsl.model.MediaTypes
 
 trait S3Utils {
 
@@ -27,15 +28,17 @@ trait S3Utils {
     source.runWith(s3Sink)
   }
 
-  def imageBytesFromS3(
-      bucket: String,
-      bucketKey: String
-  )(implicit mat: ActorMaterializer): Future[Array[Byte]] = {
-    val s3Metadata: Source[Option[ObjectMetadata], NotUsed] =
-      s3Client.getObjectMetadata(bucket, bucketKey)
-
-    s3Client.download(bucket, bucketKey)
-
-    ???
+  def imageBytesFromS3(bucket: String, bucketKey: String)(implicit mat: ActorMaterializer) = {
+    val s3File: Source[Option[(Source[ByteString, NotUsed], ObjectMetadata)], NotUsed] =
+      s3Client
+        .download(bucket, bucketKey)
+        .map {
+          case Some((source, metadata)) =>
+            val contentType = metadata.contentType
+              .flatMap(ContentType.parse(_).right.toOption)
+              .getOrElse(ContentTypes.`application/octet-stream`)
+            if (contentType == MediaTypes.`image/jpeg`) {}
+        }
   }
+
 }
