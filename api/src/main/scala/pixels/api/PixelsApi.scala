a9ec211fdb7a55b77b22e25bd05c90f75bac3a0d
Juan Marin Otero <juan.marin.otero@gmail.com>
2019-09-09 16:01:41 -0400

    Add upload route

---
+++ api/src/main/scala/pixels/api/PixelsApi.scala
@@ -0,0 +1,49 @@
+package pixels.api
+
+import akka.actor.typed.ActorSystem
+import akka.actor.typed.scaladsl.Behaviors
+import scala.concurrent.ExecutionContext
+import akka.stream.ActorMaterializer
+import akka.Done
+import akka.actor.typed.scaladsl.adapter._
+import scala.concurrent.Future
+import akka.http.scaladsl.Http
+import scala.util.{Success, Failure}
+import akka.http.scaladsl.Http.ServerBinding
+import com.typesafe.config.ConfigFactory
+
+object PixelsApi extends App with UploadRoute {
+
+  val config = ConfigFactory.load()
+
+  val name = config.getString("pixels.api.http.name")
+  val host: String = config.getString("pixels.api.http.host")
+  val port: Int = config.getInt("pixels.api.http.port")
+
+  val system = ActorSystem[Done](
+    Behaviors.setup { ctx =>
+      implicit val untypedSystem = ctx.system.toUntyped
+      implicit val materializer: ActorMaterializer = ActorMaterializer()(untypedSystem)
+      implicit val ec: ExecutionContext = ctx.system.executionContext
+
+      val routes = uploadRoute
+
+      val serverBinding: Future[ServerBinding] =
+        Http()(untypedSystem).bindAndHandle(routes, host, port)
+
+      serverBinding.onComplete {
+        case Success(bound) =>
+          println(s"Service $name running at $host:$port")
+        case Failure(e) =>
+          e.printStackTrace()
+          ctx.self ! Done
+      }
+
+      Behaviors.receiveMessage {
+        case Done => Behaviors.stopped
+      }
+
+    },
+    name
+  )
+}
---
+++ api/src/main/scala/pixels/api/UploadRoute.scala
@@ -0,0 +1,49 @@
+package pixels.api
+
+import akka.http.scaladsl.server.Route
+import akka.http.scaladsl.server.Directives._
+import akka.http.scaladsl.marshalling.ToResponseMarshallable
+import akka.http.scaladsl.model.StatusCodes
+import akka.stream.ActorMaterializer
+import scala.util.{Success, Failure}
+import pixels.s3.S3Utils
+import akka.http.scaladsl.model.MediaTypes
+
+trait UploadRoute extends S3Utils {
+
+  def uploadRoute(implicit mat: ActorMaterializer): Route =
+    pathPrefix("upload") {
+      pathEndOrSingleSlash {
+        getFromResource("web/index.html")
+      } ~
+        path("image") {
+          uploadImage
+        }
+    }
+
+  private def uploadImage(implicit materializer: ActorMaterializer): Route =
+    fileUpload("file") {
+      case (metadata, byteSource) =>
+        if (metadata.fileName.toLowerCase.endsWith(".jpg") || metadata.fileName.toLowerCase
+              .endsWith(".jpeg")) {
+
+          val fUploaded = uploadToS3(
+            byteSource,
+            "pixels-demo",
+            s"images/${metadata.fileName}",
+            MediaTypes.`image/jpeg`
+          )
+
+          onComplete(fUploaded) {
+            case Success(_) =>
+              complete(StatusCodes.Accepted)
+            case Failure(e) =>
+              e.printStackTrace()
+              complete(ToResponseMarshallable(StatusCodes.BadRequest))
+          }
+        } else {
+          complete(StatusCodes.BadRequest)
+        }
+    }
+
+}
