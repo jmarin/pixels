3e9d7d35dfc68c81b148bccd405695455a370054
Juan Marin Otero <juan.marin.otero@gmail.com>
2019-09-09 16:01:24 -0400

    Add common code

---
+++ common/src/main/scala/pixels/s3/S3Utils.scala
@@ -0,0 +1,35 @@
+package pixels.s3
+
+import akka.stream.scaladsl.{Source, Sink}
+import akka.stream.ActorMaterializer
+import scala.concurrent.Future
+import akka.util.ByteString
+import akka.stream.alpakka.s3.MultipartUploadResult
+import akka.stream.alpakka.s3.scaladsl.S3
+import akka.stream.alpakka.s3.ObjectMetadata
+import akka.NotUsed
+import akka.http.scaladsl.model.ContentType
+import akka.http.scaladsl.model.ContentTypes
+import akka.http.scaladsl.model.MediaTypes
+import akka.stream.ActorMaterializer
+
+trait S3Utils {
+
+  val s3Client = S3
+
+  def uploadToS3(
+      source: Source[ByteString, _],
+      bucket: String,
+      bucketKey: String,
+      contentType: ContentType
+  )(implicit mat: ActorMaterializer): Future[MultipartUploadResult] = {
+
+    val s3Sink: Sink[ByteString, Future[MultipartUploadResult]] =
+      s3Client.multipartUpload(bucket, bucketKey, contentType)
+    source.runWith(s3Sink)
+  }
+
+  //TODO: implement
+  def imageBytesFromS3(bucket: String, bucketKey: String) = ???
+
+}
